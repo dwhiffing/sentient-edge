ef1d33f809b153fe2cad9296add6b9ffc066c04c
Daniel Whiffing <danielwhiffing@gmail.com>
2025-01-26 15:06:11 -0500

    Adds enemy movement

--- src/entities/Enemy.ts
+++ src/entities/Enemy.ts
@@ -6,30 +6,30 @@ import { shoot } from '../utils/shoot'
 export class Enemy extends Phaser.Physics.Arcade.Sprite {
   health: number
   color: number
-  _meleeDamage: number[]
-  _rangeDamage: number[]
-  _gold: number[]
+  currentAngle: number
+  _flop: number
+  target: { x: number; y: number }
   shootRate: number
+  key: string
   justHit: boolean
+  moveEvent: Phaser.Time.TimerEvent
+  flopEvent: Phaser.Time.TimerEvent
+  spriteBody: Phaser.Physics.Arcade.Body
   declare scene: Fight
 
   constructor(scene: Fight, x: number, y: number) {
     super(scene, x, y, 'spritesheet', 0)
 
-    let flop = 0
     this.scene.add.existing(this)
     this.scene.physics.add.existing(this)
-    this.scene.time.addEvent({
-      repeat: -1,
-      delay: 300,
-      callback: () => {
-        this.y += 1 * (flop ? -1 : 1)
-        flop = flop ? 0 : 1
-      },
-    })
 
     this.setSize(16, 16)
     this.justHit = false
+    this.setOrigin(0.5, 0.5)
+    this.spriteBody = this.body as Phaser.Physics.Arcade.Body
+    this.currentAngle = 0
+    this._flop = 0
+    this.spriteBody.setCollideWorldBounds(true)
 
     if (this.shootRate > 0) {
       this.scene.time.addEvent({
@@ -40,29 +40,101 @@ export class Enemy extends Phaser.Physics.Arcade.Sprite {
     }
   }
 
+  onMoveEvent = () => {
+    const rawDistance = Phaser.Math.Distance.BetweenPoints(this, this.target)
+    const distanceRatio = Phaser.Math.Clamp(
+      rawDistance / this.stats.moveMaxDistance,
+      0,
+      1,
+    )
+    const targetBias = Math.pow(distanceRatio, 2)
+
+    const targetAngle = Math.atan2(
+      this.target.y - this.y,
+      this.target.x - this.x,
+    )
+    const randomAngle = Phaser.Math.RND.between(-180, 180)
+
+    const spreadAngle = mixAngles(
+      this.currentAngle,
+      randomAngle,
+      this.stats.moveSpreadBias,
+    )
+    this.currentAngle = mixAngles(spreadAngle, targetAngle, targetBias)
+
+    this.spriteBody.setVelocity(
+      Math.cos(this.currentAngle) * this.stats.speed,
+      Math.sin(this.currentAngle) * this.stats.speed,
+    )
+    this.setFlipX(this.spriteBody.velocity.x > 0)
+  }
+
   shoot = (count = 2, spread = 20) => {
     if (!this.active) return
 
     const p = this.scene.player
     const target = { x: p.sprite.x, y: p.sprite.y - p.spriteBody.halfHeight }
     shoot(this.scene.bullets, this, target, count, spread)
+
+    this.moveEvent = this.scene.time.addEvent({
+      delay: this.stats.moveEventDelay,
+      repeat: -1,
+      callback: this.onMoveEvent,
+    })
   }
 
   spawn(key: string) {
-    const stats = ENEMIES.find((e) => e.key === key)!
-    const x = Phaser.Math.Between(20, this.scene.cameras.main.width - 20)
-    const y = Phaser.Math.Between(20, this.scene.cameras.main.width - 20)
+    this.key = key
+    const d = this.stats.moveMaxDistance * 0.65
+    const x = Phaser.Math.Between(d, this.scene.cameras.main.width - d)
+    const y = Phaser.Math.Between(d, this.scene.cameras.main.width - d)
     this.setPosition(x, y)
 
-    this.health = Phaser.Math.RND.between(stats.health[0], stats.health[1])
-    this.shootRate = stats.shootRate
-    this._meleeDamage = stats.meleeDamage
-    this._rangeDamage = stats.rangeDamage
-    this._gold = stats.gold
-    this.setFrame(stats.frame)
-    this.color = stats.color
+    this.health = Phaser.Math.RND.between(
+      this.stats.health[0],
+      this.stats.health[1],
+    )
+    this.shootRate = this.stats.shootRate
+
+    const w = this.scene.cameras.main.width
+
+    this.target = { x: this.x, y: this.y }
+
+    if (this.stats.moveTarget === 'player') {
+      this.target = this.scene.player.sprite
+    } else if (this.stats.moveTarget === 'center') {
+      this.target = { x: w / 2, y: w / 2 }
+    } else if (this.stats.moveTarget === 'random') {
+      const y = Phaser.Math.RND.between(d, w - d)
+      const x = Phaser.Math.RND.between(d, w - d)
+      this.target = { x, y }
+    }
+
+    this.setFrame(this.stats.frame)
+    this.color = this.stats.color
     this.setTint(this.color)
-    // this.speed = stats.speed
+
+    // TODO: move to enemy stats
+    const flopRate = 300
+
+    this.moveEvent = this.scene.time.addEvent({
+      delay: this.stats.moveEventDelay,
+      repeat: -1,
+      callback: this.onMoveEvent,
+    })
+
+    this.flopEvent = this.scene.time.addEvent({
+      repeat: -1,
+      delay: flopRate,
+      callback: () => {
+        this.y += 1 * (this._flop ? -1 : 1)
+        this._flop = this._flop ? 0 : 1
+      },
+    })
+  }
+
+  get stats() {
+    return ENEMIES.find((e) => e.key === this.key)!
   }
 
   takeDamage = async (amount = 0, delay = 150) => {
@@ -73,9 +145,7 @@ export class Enemy extends Phaser.Physics.Arcade.Sprite {
     registry.set('enemyHealth', this.health)
 
     if (this.health <= 0) {
-      this.setActive(false).setVisible(false)
-      await this.delay(50)
-      this.setPosition(-20, -20)
+      this.die()
     } else {
       this.setTintFill(0xff0000)
       await this.delay(delay)
@@ -84,20 +154,48 @@ export class Enemy extends Phaser.Physics.Arcade.Sprite {
     }
   }
 
+  die = async () => {
+    this.setActive(false).setVisible(false)
+    this.moveEvent?.destroy()
+    this.flopEvent?.destroy()
+    await this.delay(50)
+    this.setPosition(-20, -20)
+  }
+
   get meleeDamage() {
-    return Phaser.Math.RND.between(this._meleeDamage[0], this._meleeDamage[1])
+    return Phaser.Math.RND.between(
+      this.stats.meleeDamage[0],
+      this.stats.meleeDamage[1],
+    )
   }
 
   get rangeDamage() {
-    return Phaser.Math.RND.between(this._rangeDamage[0], this._rangeDamage[1])
+    return Phaser.Math.RND.between(
+      this.stats.rangeDamage[0],
+      this.stats.rangeDamage[1],
+    )
   }
 
   get gold() {
-    return Phaser.Math.RND.between(this._gold[0], this._gold[1])
+    return Phaser.Math.RND.between(this.stats.gold[0], this.stats.gold[1])
   }
 
   delay = (delay: number) =>
     new Promise((resolve) => {
       this.scene.time.delayedCall(delay, resolve)
     })
+}
+
+const mixAngles = (_a1: number, _a2: number, bias: number) => {
+  const a1 = Phaser.Math.Angle.Normalize(_a1)
+  const a2 = Phaser.Math.Angle.Normalize(_a2)
+  let diff = a2 - a1
+
+  if (diff > Math.PI) {
+    diff -= 2 * Math.PI
+  } else if (diff < -Math.PI) {
+    diff += 2 * Math.PI
+  }
+
+  return Phaser.Math.Angle.Wrap(a1 + diff * Phaser.Math.Clamp(bias, 0, 1))
 }
--- src/utils/constants/enemies.ts
+++ src/utils/constants/enemies.ts
@@ -4,13 +4,33 @@ type IEnemyStats = {
   shootRate: number
   meleeDamage: number[]
   rangeDamage: number[]
-  color: number
   health: number[]
   gold: number[]
+  color: number
+
+  moveTarget: IMoveTargets
+  speed: number
+  moveEventDelay: number
+  moveSpreadBias: number
+  moveMaxDistance: number
+}
+type IMoveTargets = 'player' | 'center' | 'random' | 'spawn'
+const defaultStats = {
+  shootRate: 0,
+  health: [10, 10],
+  meleeDamage: [1, 1],
+  rangeDamage: [1, 1],
+  gold: [1, 10],
+  speed: 20,
+  moveTarget: 'center' as IMoveTargets,
+  moveEventDelay: 500,
+  moveSpreadBias: 0.2,
+  moveMaxDistance: 200,
 }
 
 export const ENEMIES: IEnemyStats[] = [
   {
+    ...defaultStats,
     color: 0xffffff,
     key: 'bug',
     frame: 24,
@@ -19,8 +39,14 @@ export const ENEMIES: IEnemyStats[] = [
     meleeDamage: [1, 4],
     rangeDamage: [1, 1],
     gold: [1, 10],
+    speed: 40,
+    moveTarget: 'spawn',
+    moveEventDelay: 200,
+    moveSpreadBias: 0.7,
+    moveMaxDistance: 50,
   },
   {
+    ...defaultStats,
     color: 0x00ff00,
     key: 'snake',
     frame: 25,
@@ -31,6 +57,7 @@ export const ENEMIES: IEnemyStats[] = [
     gold: [10, 20],
   },
   {
+    ...defaultStats,
     color: 0xffffff,
     key: 'anubis',
     frame: 26,
@@ -40,94 +67,13 @@ export const ENEMIES: IEnemyStats[] = [
     rangeDamage: [2, 4],
     gold: [100, 120],
   },
-  {
-    color: 0xffffff,
-    key: 'spider',
-    frame: 27,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    color: 0xffffff,
-    key: 'snail',
-    frame: 28,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    color: 0xffffff,
-    key: 'knight',
-    frame: 29,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    color: 0xffffff,
-    key: 'roller',
-    frame: 30,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    color: 0xffffff,
-    key: 'ogre',
-    frame: 31,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    color: 0xffffff,
-    key: 'golem',
-    frame: 32,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    color: 0xffffff,
-    key: 'zombie',
-    frame: 33,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    key: 'skeleton',
-    frame: 34,
-    shootRate: 0,
-    health: [10, 10],
-    color: 0xffffff,
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
-  {
-    color: 0xffffff,
-    key: 'death',
-    frame: 35,
-    shootRate: 0,
-    health: [10, 10],
-    meleeDamage: [1, 1],
-    rangeDamage: [1, 1],
-    gold: [1, 10],
-  },
+  { ...defaultStats, color: 0xffffff, key: 'spider', frame: 27 },
+  { ...defaultStats, color: 0xffffff, key: 'snail', frame: 28 },
+  { ...defaultStats, color: 0xffffff, key: 'knight', frame: 29 },
+  { ...defaultStats, color: 0xffffff, key: 'roller', frame: 30 },
+  { ...defaultStats, color: 0xffffff, key: 'ogre', frame: 31 },
+  { ...defaultStats, color: 0xffffff, key: 'golem', frame: 32 },
+  { ...defaultStats, color: 0xffffff, key: 'zombie', frame: 33 },
+  { ...defaultStats, color: 0xffffff, key: 'skeleton', frame: 34 },
+  { ...defaultStats, color: 0xffffff, key: 'death', frame: 35 },
 ]
