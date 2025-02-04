1faea86895ef8ed420e1564d38c5027314dac6ab
Daniel Whiffing <danielwhiffing@gmail.com>
2025-02-03 23:02:34 -0500

    Fixes game zoom to work regardless of dpr

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,34 @@ import { Stats } from './scenes/Stats'
 
 import { Game, Types } from 'phaser'
 
+const getZoom = (baseWidth: number) => {
+  const dpr = window.devicePixelRatio
+  const zoom2 = Math.round(2 * dpr) / dpr
+  const zoom3 = Math.round(3 * dpr) / dpr
+  const zoom4 = Math.round(4 * dpr) / dpr
+
+  let zoom = Math.round(dpr) / dpr
+  const dim = Math.min(window.innerWidth, window.innerHeight)
+
+  if (dim > baseWidth * zoom4) {
+    zoom = zoom4
+  } else if (dim > baseWidth * zoom3) {
+    zoom = zoom3
+  } else if (dim > baseWidth * zoom2) {
+    zoom = zoom2
+  }
+
+  return zoom
+}
+
 const config: Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 200,
   height: 200,
+  scale: { zoom: getZoom(200) },
   parent: 'game-container',
   backgroundColor: '#000000',
-  scale: {
-    zoom: Phaser.Scale.MAX_ZOOM,
-  },
-  physics: {
-    default: 'arcade',
-    arcade: {
-      // debug: true,
-    },
-  },
+  physics: { default: 'arcade' },
   pixelArt: true,
   scene: [Boot, Menu, Fight, Shop, WorldMap, CellMap, Hud, Stats],
 }
